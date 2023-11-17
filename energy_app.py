2a0f032103621ce433100b076dcd37241cc7db92
Danayt09 <danaytaman@gmail.com>
2023-11-16 20:18:50 -0500

    images

--- energy_app.py
+++ energy_app.py
@@ -4,9 +4,15 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor 
 from sklearn.model_selection import train_test_split 
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
-from sklearn.model_selection import GridSearchCV
+# from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
+import plotly.graph_objects as go
+import pickle
+import joblib
 
+st.title('Predicting Energy Consumption')
+st.header('CAISO Energy Market')
+st.sidebar.title('Energy Consumption Of SDGE By The Hour')
